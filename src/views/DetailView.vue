069e476421982e7efb1fe2e32869e628fb4e8e41
Victor Cazanave <victor.cazanave@gmail.com>
2020-07-15 15:26:13 +0800

    Clean default project

---
+++ src/views/DetailView.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>
+		Detail View
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'DetailView',
+}
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/views/ListView.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>
+		List View
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'ListView',
+}
+</script>
+
+<style lang="scss">
+</style>
