f3b276bdc6e2db07d8d93c6001c5756eb1314e5d
Victor Cazanave <victor.cazanave@gmail.com>
2020-07-16 12:21:06 +0800

    Add deployment to github pages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+	// Deploy on GitHub pages
+	// https://cli.vuejs.org/guide/deployment.html#github-pages
+	publicPath: process.env.NODE_ENV === 'production' ? '/portto-interview/' : '/',
+
+	// Access SCSS variables in all components
 	css: {
 		loaderOptions: {
 			sass: {
